85ffb22794238beaac673c5e95754e8758111a68
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2022-10-21 09:50:31 +0200

    V 2022/10/21  09:49
    Update secret key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = "9eee204f49f041e7977d9fcaf4643f04" #os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'Webeee_datas.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "static/img/posts"
