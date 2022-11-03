7d605746ac72fbe8e5a02086f882df1bc5f836e8
Ambeeee <alessandro.minestrini.01@gmail.com>
2022-11-03 16:18:17 +0100

    V 20221103 16:18
    csrf visible (shhhh)

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    try:
-        SECRET_KEY = os.environ.get("SECRET_KEY")
-    except:
-        SECRET_KEY ="9eee204f49f041e7977d9fcaf4643f04" 
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = "9eee204f49f041e7977d9fcaf4643f04" 
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'Webeee_datas.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "static/img/posts"
